31dd1f752335a79add81367167d4f90c2c45fe08
ingh8397-netizen <ingh8397@gmail.com>
2026-07-04 17:18:56 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1917,10 +1917,10 @@ async def process_mtxt_cards(event, cards, local_sites):
     status_msg = await event.reply(f"```🔥 𝙈𝙏𝙓𝙏 𝘾𝙝𝙚𝙘𝙠 𝙎𝙩𝙖𝙧𝙩𝙚𝙙 🍳 {total} 𝘾𝘾𝙎```")
 
     bin_cache = {}
-    semaphore = asyncio.Semaphore(750)  # 300 THREADS MAX - ADDED FOR FULL POWER
+    semaphore = asyncio.Semaphore(450)  # 300 THREADS MAX - ADDED FOR FULL POWER
 
     RETRY_TRIGGERS = ["merchandise_expected_price_mismatch", "unable to get payment token", "validation_custom", "invalid json response", "delivery_delivery_line_detail_changed", "status: 401", "site error", "no working site found", "products", "cloudflare", "bypass failed", "expecting value", "json", "DECISION_RULE_BLOCK", "401", "positive_amount_expected", "rate limit", "too many requests", "429", "403", "timeout", "site requires login", "site not supported", "cart failed with status 503", "connection error", "failed to get session token", "payment method not available", "invalid_payment_method", "<b>Site Error! Status: 402</b>", "delivery_address", "<b>not shopify!</b>", "no valid payment method found", "processing_error", "Cart failed with status 422", "payments_payment_flexibility_terms_id_mismatch", "SITE DEAD", "site dead", "<b>Site Error! Status: 429</b>", "proxy error: 503",
-"service unavailable", "Cart failed with status 400", "<b>Proxy Error: Server disconnected</b>", "error:", "<b>Site Error! Status: 503</b>",  "<b>Site Error! Status: 401</b>"]
+"service unavailable", "Cart failed with status 400", "<b>Proxy Error: Server disconnected</b>", "error:", "<b>Site Error! Status: 503</b>",  "<b>Site Error! Status: 401</b>", "HTTP_ERROR_502"]
 
     # ADDED: Hit tracker to guarantee zero missed results
     hit_cards = set()
@@ -2021,7 +2021,7 @@ async def process_mtxt_cards(event, cards, local_sites):
                         pass
 
                     percent = min(round((checked / total) * 100, 1), 100)
-                    blocks = 50
+                    blocks = 15
                     filled = int((checked / total) * blocks)
                     bar = "█" * filled + "░" * (blocks - filled)
 
@@ -2040,7 +2040,7 @@ async def process_mtxt_cards(event, cards, local_sites):
                         [Button.inline("🛑 𝗦𝗧𝗢𝗣", f"stop_mtxt:{user_id}".encode())]
                     ]
 
-                    if checked % 15 == 0 or checked == total:  # Faster updates
+                    if checked % 50 == 0 or checked == total:  # Faster updates
                         try:
                             await status_msg.edit(status_text, buttons=buttons)
                         except:
